2cf22fdc7bcb603e3af76f1f953e7289aeae5674
Anton Potapchuk <anton95@rocket.hpc.ut.ee>
2018-09-08 01:09:31 +0300

    Fixed lovecz loss

--- src/models/losses.py
+++ src/models/losses.py
@@ -73,24 +73,26 @@ def __lovasz_hinge_flat(logits, labels):
         grad = __lovasz_grad(gt_sorted)
         loss = tf.tensordot(tf.nn.relu(errors_sorted), tf.stop_gradient(grad), 1, name="loss_non_void")
         return loss
+    loss = tf.cond(tf.equal(tf.shape(logits)[0], 0),
+                   lambda: tf.reduce_sum(logits) * 0.,
+                   compute_loss,
+                   strict=True,
+                   name="loss"
+                  )
+    return loss
 
 
-def __flatten_binary_scores(scores, labels, ignore=None):
+def __flatten_binary_scores(scores, labels):
     """
     Flattens predictions in the batch (binary case)
     Remove labels equal to 'ignore'
     """
     scores = tf.reshape(scores, (-1,))
     labels = tf.reshape(labels, (-1,))
-    if ignore is None:
-        return scores, labels
-    valid = tf.not_equal(labels, ignore)
-    vscores = tf.boolean_mask(scores, valid, name='valid_scores')
-    vlabels = tf.boolean_mask(labels, valid, name='valid_labels')
-    return vscores, vlabels
+    return scores, labels
 
 
-def __lovasz_hinge(logits, labels, per_image=True, ignore=None):
+def __lovasz_hinge(logits, labels):
     """
     Binary Lovasz hinge loss
       logits: [B, H, W] Variable, logits at each pixel (between -\infty and +\infty)
@@ -98,21 +100,19 @@ def __lovasz_hinge(logits, labels, per_image=True, ignore=None):
       per_image: compute the loss per image instead of per batch
       ignore: void class id
     """
-    if per_image:
-        def treat_image(log_lab):
-            log, lab = log_lab
-            log, lab = tf.expand_dims(log, 0), tf.expand_dims(lab, 0)
-            log, lab = __flatten_binary_scores(log, lab, ignore)
-            return __lovasz_hinge_flat(log, lab)
-        losses = tf.map_fn(treat_image, (logits, labels), dtype=tf.float32)
-        loss = tf.reduce_mean(losses)
-    else:
-        loss = __lovasz_hinge_flat(*__flatten_binary_scores(logits, labels, ignore))
+    def treat_image(log_lab):
+        log, lab = log_lab
+        log, lab = tf.expand_dims(log, 0), tf.expand_dims(lab, 0)
+        log, lab = __flatten_binary_scores(log, lab)
+        return __lovasz_hinge_flat(log, lab)
+    losses = tf.map_fn(treat_image, (logits, labels), dtype=tf.float32)
+    losses.set_shape((None,))
+    loss = tf.reduce_mean(losses)
     return loss
 
 
 def keras_lovasz_hinge(labels,logits):
-    return __lovasz_hinge(logits, labels, per_image=True, ignore=None)
+    return __lovasz_hinge(logits, labels)
 
 
 # My loss
